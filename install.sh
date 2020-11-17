0a6c4124a2c842242058b73697343bc043af49ad
dhan2code <adhan001@gmail.com>
2020-11-17 21:13:14 +0530

    initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ln -s ~/Dotfiles/vim/vimrc ~/.vimrc
+
+ln -s ~/Dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -s ~/Dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -s ~/Dotfiles/ranger/devicon.py ~/.config/ranger/plugin/devicon.py
+ln -s ~/Dotfiles/ranger/__init__.py ~/.config/ranger/plugin/__init__.py
+
+ln -s ~/Dotfiles/bash/bashrc ~/.bashrc
+
+ln -s ~/Dotfiles/sxiv/key-handler ~/.config/sxiv/exec/key-handler
+
+ln -s ~/Dotfiles/zathura/zathurarc ~/.config/zathura/zathurarc
+
+cp -r ~/Dotfiles/vim/UltiSnips ~/.vim/Ultisnips
