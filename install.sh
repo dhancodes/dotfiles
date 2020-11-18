e6a1d198718343bffdefdc14d368720840631c19
dhan2code <adhan001@gmail.com>
2020-11-18 18:35:51 +0530

    adding install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+#Place the Dotfiles folder on your home directory.
+#Run the script.
 
-ln -s ~/Dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/Dotfiles/vim/vimrc ~/.vimrc #for vim
 
-ln -s ~/Dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -s ~/Dotfiles/ranger/rifle.conf ~/.config/ranger/rifle.conf #for ranger
 ln -s ~/Dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
 ln -s ~/Dotfiles/ranger/devicon.py ~/.config/ranger/plugin/devicon.py
 ln -s ~/Dotfiles/ranger/__init__.py ~/.config/ranger/plugin/__init__.py
 
-ln -s ~/Dotfiles/bash/bashrc ~/.bashrc
+ln -s ~/Dotfiles/bash/bashrc ~/.bashrc #For bash
 
-ln -s ~/Dotfiles/sxiv/key-handler ~/.config/sxiv/exec/key-handler
+ln -s ~/Dotfiles/sxiv/key-handler ~/.config/sxiv/exec/key-handler #For sxiv
 
-ln -s ~/Dotfiles/zathura/zathurarc ~/.config/zathura/zathurarc
-
-cp -r ~/Dotfiles/vim/UltiSnips ~/.vim/Ultisnips
+ln -s ~/Dotfiles/zathura/zathurarc ~/.config/zathura/zathurarc #For zathura
